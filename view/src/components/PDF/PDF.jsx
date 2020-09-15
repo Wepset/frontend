79f7c36d53c6bd745ee84f2052696b7f26a7d536
Wesley Flôres <wesleyfloresterres@gmail.com>
2020-09-14 23:00:31 -0300

    PDF

---
+++ view/src/components/PDF/PDF.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import PDFIcon from './../../images/export-pdf.png';
+
+function PDF() {
+    return (
+        <img src={PDFIcon} alt="Pdf Icon" />
+    );
+}
+
+export default PDF;
