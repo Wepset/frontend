924b7060dd1770f202d035fbf1f3f615a6d5f9ef
Wesley Flôres <wesleyfloresterres@gmail.com>
2020-09-14 23:00:24 -0300

    CSV

---
+++ view/src/components/CSV/CSV.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import CSVIcon from './../../images/export-csv.png';
+
+function CSV() {
+    return (
+        <img src={CSVIcon} alt="Csv Icon" />
+    );
+}
+
+export default CSV;
