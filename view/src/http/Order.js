47d6ea99a3524bcec51361078aa606c90e70ec5d
Wesley Flôres <wesleyfloresterres@gmail.com>
2020-09-16 00:05:45 -0300

    Order.js

--- view/src/http/Order.js
+++ view/src/http/Order.js
@@ -1,7 +1,13 @@
 import api from './../service/api.js';
 
 class Order {
+    create(id) {
+        return api.post('orders', { id: id });
+    }
 
+    index() {
+        return api.get('orders');
+    }
 }
 
 export default Order;
