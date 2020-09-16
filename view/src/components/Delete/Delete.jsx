c2ce6e6f275eca92780d814dfda3308bd6113a12
Wesley Flôres <wesleyfloresterres@gmail.com>
2020-09-16 19:42:55 -0300

    Delete

---
+++ view/src/components/Delete/Delete.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import img from '../../images/db_icones.png';
+
+import './Delete.css';
+
+function Delete() {
+    return (
+        <img src={img} className="icon L2H1" alt="Delete"></img>
+    );
+}
+
+export default Delete;
