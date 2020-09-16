7831fdb94b4ae15d54fb0c8924dbbf0394805962
Wesley Flôres <wesleyfloresterres@gmail.com>
2020-09-16 19:43:06 -0300

    Whatsapp

---
+++ view/src/components/Whatsapp/Whatsapp.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import img from '../../images/db_icones.png';
+
+import './Whatsapp.css';
+
+function Whatsapp() {
+    return (
+        <img src={img} className="icon L7H1" alt="Whatsapp"></img>
+    );
+}
+
+export default Whatsapp;
