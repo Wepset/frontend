788e77bb4faa10829a8d349d6398b7dd6f705524
Wesley Flôres <wesleyfloresterres@gmail.com>
2020-09-16 00:05:26 -0300

    migrations

--- server/database/migrations/2020_09_11_185313_create_orders_table.php
+++ server/database/migrations/2020_09_11_185313_create_orders_table.php
@@ -15,7 +15,8 @@ class CreateOrdersTable extends Migration
     {
         Schema::create('orders', function (Blueprint $table) {
             $table->id();
-            $table->unsignedBigInteger('product_id');
+            $table->unsignedBigInteger('quantity', false);
+            $table->unsignedBigInteger('product_id')->unique();
             $table->foreign('product_id')->references('id')->on('products');
             $table->timestamps();
         });
