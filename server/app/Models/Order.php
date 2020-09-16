ea788c5f4bc8906dba32f831012922d328c1a513
Wesley Flôres <wesleyfloresterres@gmail.com>
2020-09-16 00:05:19 -0300

    Order

--- server/app/Models/Order.php
+++ server/app/Models/Order.php
@@ -7,5 +7,20 @@ use Illuminate\Database\Eloquent\Model;
 
 class Order extends Model
 {
+    /** @var array */
+    protected $fillable = ['product_id'];
+
+    /**
+     * Products.
+     * 
+     * @param void
+     * 
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function products(): \Illuminate\Database\Eloquent\Relations\HasMany
+    {
+        return $this->hasMany(\App\Models\Product::class);
+    }
+
     use HasFactory;
 }
