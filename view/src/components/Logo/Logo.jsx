e6e1f432543f663327131cb414e52b549b242cf8
Wesley Flôres <wesleyfloresterres@gmail.com>
2020-09-16 00:52:12 -0300

    Logo

---
+++ view/src/components/Logo/Logo.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import LogoIcon from './../../images/2020-09-16.png';
+
+function Logo() {
+    return (
+        <img src={LogoIcon} alt="Logo" />
+    );
+}
+
+export default Logo;
