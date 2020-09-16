3448e966b76c9c5aeb8c7f5477c1888b0a19cf28
Wesley Flôres <wesleyfloresterres@gmail.com>
2020-09-16 00:06:03 -0300

    Updated Sales.jsx

--- view/src/pages/Sales/Sales.jsx
+++ view/src/pages/Sales/Sales.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import * as ReactDOM from 'react-dom';
 
@@ -6,56 +6,82 @@ import { Form, Button, Container, Image } from 'react-bootstrap';
 import { FiZoomIn } from 'react-icons/fi';
 import { AiOutlineSolution } from 'react-icons/ai';
 
-import Logo from './../../images/alfa-sistemas.png';
+import Logo from './../../images/2020-09-16.png';
 
 import Consumer from '../../components/Consumer/Consumer.jsx';
 import Product from '../../components/Product/Product.jsx';
+import OrderGrid from '../../components/OrderGrid/OrderGrid.jsx';
 
 import './Sales.css';
 
 function Sales() {
+    const [seller, setSeller] = useState('');
+    const [consumer, setConsumer] = useState('');
+
+    /**
+     * handleSeller
+     * 
+     * @param {Object} e 
+     */
+    function handleSeller(e) {
+        setSeller(e.target.value);
+    }
+
+    /**
+     * handleConsumer
+     * 
+     * @param {Object} e 
+     */
+    function handleConsumer(e) {
+        setConsumer(e.target.value);
+    }
+
     return (
-        <Container>
-            <Form.Row className="d-flex flex-nowrap">
-                <>
-                    <Form.Group as={Form.Col}>
-                        <Form.Label className="mr-2">Cliente</Form.Label>
-                        <Form.Control type="text" placeholder="Informe o Cliente" />
+        <>
+            <Container>
+                <Form.Row className="d-flex flex-nowrap">
+                    <>
+                        <Form.Group as={Form.Col}>
+                            <Form.Label className="mr-2">Cliente</Form.Label>
+                            <Form.Control type="text" value={consumer} onChange={handleConsumer} placeholder="Informe o Cliente" />
+                        </Form.Group>
+
+                        <Form.Group className="d-flex align-items-end col-md-1">
+                            <span onClick={() => { ReactDOM.render(<Consumer />, document.querySelector("#modal")) }}>
+                                <h3> <FiZoomIn /></h3>
+                            </span>
+                            <h3> <AiOutlineSolution /></h3>
+                        </Form.Group>
+                    </>
+
+                    <>
+                        <Form.Group as={Form.Col}>
+                            <Form.Label>Vendedor</Form.Label>
+                            <Form.Control type="text" value={seller} onChange={handleSeller} placeholder="Informe o Vendedor" />
+                        </Form.Group>
+
+                        <Form.Group className="d-flex align-items-end col-md-1">
+                            <span onClick={() => { ReactDOM.render(<Consumer />, document.querySelector("#modal")) }}>
+                                <h3> <FiZoomIn /></h3>
+                            </span>
+                            <h3> <AiOutlineSolution /></h3>
+                        </Form.Group>
+                    </>
+
+                    <Form.Group className="d-flex align-items-end col-md-2">
+                        <Button variant="primary" onClick={() => { ReactDOM.render(<Product />, document.querySelector("#modal")) }}>
+                            Pesquisar item
+                        </Button>
                     </Form.Group>
 
-                    <Form.Group className="d-flex align-items-end col-md-1">
-                        <span onClick={() => { ReactDOM.render(<Consumer />, document.querySelector("#modal")) }}>
-                            <h3> <FiZoomIn /></h3>
-                        </span>
-                        <h3> <AiOutlineSolution /></h3>
+                    <Form.Group className="d-flex align-items-center">
+                        <Image src={Logo} alt="Logo" className="rounded float-right img-fluid" style={{ "height": "70px" }} />
                     </Form.Group>
-                </>
+                </Form.Row>
+            </Container >
 
-                <>
-                    <Form.Group as={Form.Col}>
-                        <Form.Label>Vendedor</Form.Label>
-                        <Form.Control type="text" placeholder="Informe o Vendedor" />
-                    </Form.Group>
-
-                    <Form.Group className="d-flex align-items-end col-md-1">
-                        <span onClick={() => { ReactDOM.render(<Consumer />, document.querySelector("#modal")) }}>
-                            <h3> <FiZoomIn /></h3>
-                        </span>
-                        <h3> <AiOutlineSolution /></h3>
-                    </Form.Group>
-                </>
-
-                <Form.Group className="d-flex align-items-end col-md-2">
-                    <Button variant="primary" onClick={() => { ReactDOM.render(<Product />, document.querySelector("#modal")) }}>
-                        Pesquisar item
-                    </Button>
-                </Form.Group>
-
-                <Form.Group className="d-flex align-items-center">
-                    <Image src={Logo} alt="Logo" className="rounded float-right img-fluid" style={{ "height": "70px" }} />
-                </Form.Group>
-            </Form.Row>
-        </Container >
+            <OrderGrid />
+        </>
     );
 }
 
